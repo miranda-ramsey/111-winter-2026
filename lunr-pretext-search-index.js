64694595bf0213b0c6f476489e8d81291c4dd252
Miranda Ramsey <miranda.j.ramsey@gmail.com>
2025-12-23 11:46:46 -0800

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,122 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "  Miranda Ramsey Portland Community College      My Website   copyright         "
+},
+{
+  "id": "sec-course-information",
+  "level": "1",
+  "url": "sec-course-information.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Course Information",
+  "body": " Course Information      Title  Precalculus I: Functions    Number  MTH111Z    CRN  11221    Credits  4    Term  Winter 2026    Prerequisites  MTH 95 and (RD 115 and WR 115) or IRW 115 or equivalent placement.    Times  Tuesdays and Thursdays, 9:00AM - 11:50AM    Location  In Person, TABOR 143, Southeast Campus       Course Description   A course primarily designed for students preparing for trigonometry or calculus. This course focuses on functions and their properties, including polynomial, rational, exponential, logarithmic, piecewise-defined, and inverse functions. These topics will be explored symbolically, numerically, and graphically in real-life applications and interpreted in context. This course emphasizes skill building, problem solving, modeling, reasoning, communication, connections with other disciplines, and the appropriate use of present-day technology. This course is part of Oregon Common Course Numbering. MTH 111 and MTH 111Z are equivalent at PCC. The PCC Mathematics Department recommends that students take MTH courses in consecutive terms. Audit available. Read the Course Content and Outcomes Guide here .    Learning Outcomes  Upon successful completion of this course, students should be able to:  Explore the concept of a function numerically, symbolically, verbally, and graphically and identify properties of functions both with and without technology.  Analyze polynomial, rational, exponential, and logarithmic functions, as well as piecewise-defined functions, in both algebraic and graphical contexts, and solve equations involving these function types.  Demonstrate algebraic and graphical competence in the use and application of functions including notation, evaluation, domain\/range, algebraic operations and composition, inverses, transformations, symmetry, rate of change, extrema, intercepts, asymptotes, and other behavior.  Use variables and functions to represent unknown quantities, create models, find solutions, and communicate an interpretation of the results.  Determine the reasonableness and implications of mathematical methods, solutions, and approximations in context.      "
+},
+{
+  "id": "sec-instructor",
+  "level": "1",
+  "url": "sec-instructor.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Instructor Information",
+  "body": " Instructor Information      Instructor  Miranda Jane Ramsey    Email   miranda.ramsey@pcc.edu     Phone  971-722-3542    Office Location  SCOM 214    School Address  Portland Community College, 12000 SW 49th Ave., Portland, OR 97219        Communication   Best Way to Contact Me  I can best be reached by email. I try to reply the same day when I can, and I will generally reply before the end of the next school day.     Schedule  This term I am teaching the following classes:   Term Schedule           Class  CRN  Days  Time  Place  Midterm  Final    STAT 243Z  13446  MW  9:00 11:50  TABOR 142  February 9  March 16    MTH 58  11475  MW  2:00 4:20  SCOM 314   February 4  February 25   March 18    MTH 111Z  11221  TuTh  9:00 11:50  TABOR 143  February 5  March 12    MTH 231  -  TuTh  1:00 3:40  Zoom        Office Hours       Day  Time  Place    Mondays  12-1  SCOM 214    Wednesdays  12-1  SCOM 214    Fridays  By Appointment  Zoom      "
+},
+{
+  "id": "sec-instructor-4-3",
+  "level": "2",
+  "url": "sec-instructor.html#sec-instructor-4-3",
+  "type": "Table",
+  "number": "2.1",
+  "title": "Term Schedule",
+  "body": " Term Schedule           Class  CRN  Days  Time  Place  Midterm  Final    STAT 243Z  13446  MW  9:00 11:50  TABOR 142  February 9  March 16    MTH 58  11475  MW  2:00 4:20  SCOM 314   February 4  February 25   March 18    MTH 111Z  11221  TuTh  9:00 11:50  TABOR 143  February 5  March 12    MTH 231  -  TuTh  1:00 3:40  Zoom      "
+},
+{
+  "id": "sec-instructor-4-4",
+  "level": "2",
+  "url": "sec-instructor.html#sec-instructor-4-4",
+  "type": "Table",
+  "number": "2.2",
+  "title": "Office Hours",
+  "body": " Office Hours       Day  Time  Place    Mondays  12-1  SCOM 214    Wednesdays  12-1  SCOM 214    Fridays  By Appointment  Zoom    "
+},
+{
+  "id": "sec-materials",
+  "level": "1",
+  "url": "sec-materials.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Materials",
+  "body": " Materials   Textbooks  Our primary textbook will be Openstax Algebra and Trigonometry 2e . This is a free textbook you can access online. You may purchase a paper copy for a small price, if you want.    Supplies  In this class, you will need:   Pencils with erasers    Note paper of your choice     Engineering paper for completing your homework write-ups    Ruler, straightedge, or protractor for drawing neat graphs. The bookstore has a bunch of nice options.       Software\/Hardware\/Equipment  We will be using a number of free software options in this class. To access these outside of our classroom, you will need to have access to a personal computer with a recommended browser installed. We will be accessing the following:    D2L Brightspace , our primary learning platform with PCC,     Desmos , a free online graphing calculator,     MyOpenMath , where you will complete weekly homework assignments and quizzes, and     Amplify Classroom , where we will occasionally complete lab activities.     Please visit the technology support section of Student Support Resources for a complete list of apps and tools for PCC students.    Accessibility  This course uses digital courseware, software, or texts that may cause barriers for those using assistive, or accessibility-related technologies. If you encounter barriers, please contact the Access Tech Team at access-tech-group@pcc.edu or by calling 971-722-8324.   "
+},
+{
+  "id": "sec-attendance",
+  "level": "1",
+  "url": "sec-attendance.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Attendance and Participation",
+  "body": " Attendance and Participation   Students are expected to attend each scheduled class period. Credit will be given each day for:   Arriving on time with all necessary materials. (2 points)    Engaging with the group during the lab portion. (2 points)    Staying for the full duration of the class. (1 point)       First Week Activities  During the first week of the term, instructors must record students who are not in attendance or engaging in the class. Students who have not done the following will be dropped according to PCC's No Show Policy :   Attend all of the first days of class up to the drop deadline.    Enroll in the MyOpenMath course and complete the first week's assignments.    Complete and submit the first week's lab assignments.   If you are unable to complete one of these requirements but want to remain enrolled in the class, you must speak to me to make an alternative arrangement to show engagement in the course.   "
+},
+{
+  "id": "sec-grading",
+  "level": "1",
+  "url": "sec-grading.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Grading",
+  "body": " Grading   For more information, please go to the PCC Grading Guidelines .    Graded Assignments     Attendance  Each day of class you will receive a grade based on arriving on time, having all of your required course materials, engaging and participating during the lab time, and staying until the end of class.    Homework  Each week will have a homework assignment on MyOpenMath (often split into two question sets, e.g. Homework 1a and Homework 1b) and a quiz. You will complete the homework in two parts.   First, you will answer the questions in MyOpenMath. The questions are set up to give you as many chances as you need to get the right answer. Many questions have supportive videos or explanations to help you through the problem, or another one like it. You can keep trying until you get all of the answers right.   Once you have gotten all of the correct answers, you will complete a homework write-up on paper. See the homework and lab guidelines for instructions for completing the homework write-up.   Quizzes are available to help you prepare for the exam. You are encouraged to do quizzes without book, notes, or other outside help. You may take the quiz up to three times, keeping the highest score.    Lab Work  Each day after a lecture covering the material, you will complete an activity with your group. For full credit, you will write up a conclusion to the activity using the same guidelines as the homework and submit at the end of class. See the homework and lab guidelines for instructions for completing the homework write-up.    Exams  There will be two exams.  The midterm exam will be on February 5 during our normal class time. It will cover functions in general. The final exam will be on March 12 during our normal class time. It will cover exponential functions, logarithmic functions, polynomials, rational functions, and models.  Exams are closed book and no notes. Scientific calculators will be allowed for making small calculations.    Final Project  After the midterm exam, students will be invited to form groups to work on a final project. Some class time will be available to work on the project together, and presentations will be given during our scheduled Final's week meeting.  Projects will be student led inquiries into a particular function or function family that exists within a chosen field. Depending on the function chosen, the exploration will include example data; historical context; interpretations of domains, extrema, and other features; and other topics of discussion that relate the concepts learned in class to a mathematical function in the real world.  Some example topics include Ohm's Law, carrying capacity, carbon dating, taxation, cryptography, or a preview into trigonometry.       Homework and Lab Guidelines  To be filled.    Late Work and Make-Up Policy  Students are expected to complete all assignments on pace with the course. However, I understand that external things get in the way. On MyOpenMath, students are given six \"latepasses\" which they may use to apply extensions to their work.   All homework assignments must be completed before the final exam, regardless of late passes.   For assignments without late passes, I will consider giving up to 1 week extension when requested before the original due date of the assignment.  If you must miss an exam, you must arrange a make up exam with me before the day of the exam.    Criteria  Please read over all assignments and discussion prompts in advance and reach out if you have any questions or if you want to confirm you understand them correctly. Clarifying questions will help you avoid deductions in your grades.       Activities  Number  Weight    Attendance  21  5%    Lab  17  15%    Homework  9  20%    Project  1  10%    Midterm Exam  1  25%    Final Exam  1  25%         Letter Grade  Percentage    A  90-100%    B  80-89%    C  70-79%    D  60-69%    F  < 59%      "
+},
+{
+  "id": "sec-policies",
+  "level": "1",
+  "url": "sec-policies.html",
+  "type": "Section",
+  "number": "6",
+  "title": "PCC Policies and Deadlines",
+  "body": " PCC Policies and Deadlines   Visit the PCC Policies page for information on:   Academic Integrity Policy    Accessibility Statement and Resources    Drop\/Withdraw Deadlines    Grading Policy    Internet Etiquette    PCC Payment Policy    PCC Registration Policy    Sanctuary College    Student Rights and Responsibilities    Title IX\/Non-Discrimination       Drop\/Withdraw Deadlines  A student is responsible to add, drop, or withdraw classes. Please review PCC Registration Policy for more information.   Students need to register online via MyPCC. Please review the Online Registration Instructions to find out how.    The last day to drop this class is Tuesday, Jan 13 . This is also the last day to register with instructor approval.    The last day to withraw is Saturday, March 14 .       Accomodations and Services  PCC is committed to supporting all students. If you plan to use academic accommodations for this course, please contact your instructor as soon as possible to discuss your needs. Accommodations are not retroactive; they begin when the instructor receives the “Approved Academic Accommodations” letter from you (submitted in person for courses on campus; via email for Online Learning courses). To request academic accommodations for a disability, please contact an Accessible Education and Disability Resources counselor on any PCC campus. Office locations, phone numbers, and additional information may be located on the Accessible Education and Disability Resources website .    Student Rights and Responsibilities  Students are required to complete this course in accordance with the Student Rights and Responsibilities Handbook . The Handbook establishes students' freedoms and protections as well as expectations of appropriate behavior and ethical academic work. The Handbook includes items such as the Policy on Student Rights, and the Student Code of Conduct Policy and Procedures.    Academic Integrity   Rules about cheating, plagiarism, or sharing work  The handbook contains the Code of Student Conduct and the Academic Integrity Policy. Cheating includes any attempt to defraud, deceive, or mislead the instructor in arriving at an honest grade assessment, and may include copying answers from other students or using unauthorized notes during tests. Plagiarism is a particular form of cheating that involves presenting as one's own the ideas or work of another and may include using other people's ideas without proper attribution and submitting another person's work as one's own. Dishonest activities such as cheating on exams and submitting or copying work done by others will result in disciplinary actions including but not limited to receiving a failing grade. For further information, review the institution's Academic Integrity Policy .    Generative AI  The use of generative AI tools, including but not limited to ChatGPT, to complete your work is strictly prohibited and will be considered equivalent to unauthorized sharing of work or plagiarism. All AI use is strongly discouraged, even as a study tool. I want to help you build a strong internal understanding of the material without relying on outside tools. If you have a question or need help, please reach out to me instead.  In return, I promise that I will not use generative AI in my relationship with you as your instructor. Any feedback you receive on your work will be my authentic human response to you.     Internet Etiquette  Some of this class may include online discussions or other interactions with students online. Click here for more information about Internet Etiquette (or Netiquette) .    Sanctuary College Statement  PCC is a sanctuary college. For more information and resources, see the resources for undocumented students page .    Inclement Weather Statement  Our course is typically unaffected by college campus and facility closures or delayed class start times. Our regular due dates apply. However, if PCC must close all operations, our regular due dates will be adjusted accordingly. Please check your email for my instructions and continue to check D2L Brightspace during closures.    Flexibility Statement  The instructor reserves the right to modify course content and\/or substitute assignments and learning activities in response to institutional, weather, or class situations.   "
+},
+{
+  "id": "sec-calendar",
+  "level": "1",
+  "url": "sec-calendar.html",
+  "type": "Section",
+  "number": "7",
+  "title": "Calendar",
+  "body": " Calendar  This schedule is a tentative outline and may change to meet the needs of the class.   Course Calendar        Week  Tuesday  Thursday  Assignments    1   January 6  Algebra Review    January 8  Graphing Review    Homework 1  Quiz 1     2   January 13  Functions    January 15  Domain and Range    Homework 2  Quiz 2     3   January 20  Function Behaviors    January 22  Function Arithmetic    Homework 3  Quiz 3     4   January 27  Outside Transformations    January 29  Inside Transformations    Homework 4  Quiz 4     5   February 3  Inverse Functions    February 5  Midterm Exam    Homework 5  Quiz 5     6   February 10  Exp and Logs Review    February 12  and the Natural Log    Homework 6  Quiz 6     7   February 17  Exponential Functions    February 19  Logarithmic Functions    Homework 7  Quiz 7     8   February 24  Polynomials    February 26  Rational Functions    Homework 8  Quiz 8     9   March 3  Interpreting Models    March 5  Creating Models    Homework 9  Quiz 9     10   March 10  Review    March 12  Final Exam   -    Finals   March 17  Presentations    March 19  No Class   -     "
+},
+{
+  "id": "sec-calendar-3",
+  "level": "2",
+  "url": "sec-calendar.html#sec-calendar-3",
+  "type": "Table",
+  "number": "7.1",
+  "title": "Course Calendar",
+  "body": " Course Calendar        Week  Tuesday  Thursday  Assignments    1   January 6  Algebra Review    January 8  Graphing Review    Homework 1  Quiz 1     2   January 13  Functions    January 15  Domain and Range    Homework 2  Quiz 2     3   January 20  Function Behaviors    January 22  Function Arithmetic    Homework 3  Quiz 3     4   January 27  Outside Transformations    January 29  Inside Transformations    Homework 4  Quiz 4     5   February 3  Inverse Functions    February 5  Midterm Exam    Homework 5  Quiz 5     6   February 10  Exp and Logs Review    February 12  and the Natural Log    Homework 6  Quiz 6     7   February 17  Exponential Functions    February 19  Logarithmic Functions    Homework 7  Quiz 7     8   February 24  Polynomials    February 26  Rational Functions    Homework 8  Quiz 8     9   March 3  Interpreting Models    March 5  Creating Models    Homework 9  Quiz 9     10   March 10  Review    March 12  Final Exam   -    Finals   March 17  Presentations    March 19  No Class   -    "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
